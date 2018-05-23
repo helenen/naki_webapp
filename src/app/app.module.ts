69364538060dd9de327c121e3d5d2c92ea8479fd
helenen <helene.niassy@gmail.com>
2018-05-23 17:04:38 +0200

    add path and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
+import { AppComponent } from "./app.component";
+import { LevelComponent } from "./components/level/level.component";
+import { MenuComponent } from "./components/menu/menu.component";
+import { ContactComponent } from "./components/contact/contact.component";
 
-import { AppComponent } from './app.component';
-
-
+const appRoutes: Routes = [
+  { path: "level", component: LevelComponent },
+  { path: "contact", component: ContactComponent }
+];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LevelComponent, MenuComponent, ContactComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
