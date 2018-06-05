03df014359db7037329660bfb1decfc8150abe40
helenen <helene.niassy@gmail.com>
2018-06-05 18:44:03 +0200

    get back exercises data

--- src/app/components/topic/topic.component.ts
+++ src/app/components/topic/topic.component.ts
@@ -1,23 +1,23 @@
-import { TopicService } from './topic.service';
-import { Component, OnInit } from '@angular/core';
-import 'rxjs/add/operator/map';
-
+import { TopicService } from "./topic.service";
+import { Component, OnInit } from "@angular/core";
+import "rxjs/add/operator/map";
 
 @Component({
-  selector: 'app-topic',
-  templateUrl: './topic.component.html',
-  styleUrls: ['./topic.component.css']
+  selector: "app-topic",
+  templateUrl: "./topic.component.html",
+  styleUrls: ["./topic.component.css"]
 })
 export class TopicComponent implements OnInit {
-private apiUrl = 'http://localhost:8080/lessons';
-data: any = {};
-
   constructor(private topicService: TopicService) {
-    this.topicService.getLessons()
-      .subscribe(data => {
-        console.log(data);
-      });
-    }
-    ngOnInit() {
-    }
+    this.topicService.getLessons().subscribe(data => {
+      console.log(data);
+    });
+    this.topicService.getCours().subscribe(data => {
+      console.log(data);
+    });
+    this.topicService.getExercises().subscribe(data => {
+      console.log(data);
+    });
   }
+  ngOnInit() {}
+}
--- src/app/components/topic/topic.service.ts
+++ src/app/components/topic/topic.service.ts
@@ -1,16 +1,27 @@
-import { Injectable } from '@angular/core';
-import {Http, Response} from '@angular/http';
-import 'rxjs/add/operator/map';
-
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+import "rxjs/add/operator/map";
+import { environment } from "../../../environments/environment";
 
 @Injectable()
 export class TopicService {
-private apiUrl = 'http://localhost:8080/lessons';
+  private apiUrl = environment.api_Url;
 
-  constructor(private http: Http) { }
+  constructor(private http: Http) {}
 
-  getLessons(){
-    return this.http.get(this.apiUrl)
+  getLessons() {
+    return this.http
+      .get(this.apiUrl + "/lessons")
+      .map((res: Response) => res.json());
+  }
+  getCours() {
+    return this.http
+      .get(this.apiUrl + "/cours")
+      .map((res: Response) => res.json());
+  }
+  getExercises() {
+    return this.http
+      .get(this.apiUrl + "/exercises")
       .map((res: Response) => res.json());
   }
 }
