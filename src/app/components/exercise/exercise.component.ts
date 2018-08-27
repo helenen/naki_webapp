a2ee73f63e23cb31a876b34ccbca70aa8426ca7c
helenen <helene.niassy@gmail.com>
2018-08-27 21:28:13 +0200

    begin add sound

--- src/app/components/exercise/exercise.component.ts
+++ src/app/components/exercise/exercise.component.ts
@@ -1,3 +1,4 @@
+import { Exercise } from "./../../models/exercise";
 import { Component, OnInit } from "@angular/core";
 import { ExerciseService } from "./exercise.service";
 import { Asset } from "../../models/asset";
@@ -12,6 +13,7 @@ export class ExerciseComponent implements OnInit {
   assets: Asset;
   audio: any;
   exerciseId: number;
+  assetsByExercise: Exercise;
 
   constructor(
     private exerciseService: ExerciseService,
@@ -20,6 +22,15 @@ export class ExerciseComponent implements OnInit {
 
   ngOnInit() {
     this.exerciseId = +this.route.snapshot.params.exerciseId;
+
+    this.route.params.map(params => params["id"]).subscribe(id => {
+      this.exerciseService
+        .getAssetsByExercise(this.exerciseId)
+        .subscribe(assetsByExercise => {
+          this.assetsByExercise = assetsByExercise;
+          console.log(this.assetsByExercise, "po");
+        });
+    });
     this.audio = new Audio();
     this.audio.src = this.audio.play();
   }
